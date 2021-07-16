a7b7a96062c5e43c6f670da73be25e5cd95c6d03
Elkin Fabian Rozo Ortegón <elkin.rozo@grupobabel.com>
2021-07-16 10:46:39 -0500

    ajuste http

--- src/main/java/co/com/routes/RouteProcess.java
+++ src/main/java/co/com/routes/RouteProcess.java
@@ -125,7 +125,7 @@ public class RouteProcess extends RouteBuilder{
 			.process( x ->{
 				 SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
 				 sslContext.init(null, null, null);
-			      SSLConnectionSocketFactory f = new SSLConnectionSocketFactory(sslContext, new String[] { "TLSv1.2" }, null,
+			      SSLConnectionSocketFactory f = new SSLConnectionSocketFactory(sslContext, new String[] { "TLSv1", "TLSv1.1", "TLSv1.2" }, null,
 			                                        SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
 			 
 			      Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
